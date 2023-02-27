e06fdc815428d3890a928108ef40499702de80af
bruno.eleazaro <eleazaro.bruno@gmail.com>
2023-02-27 14:10:53 -0300

    new route aluno matricula

--- routes/alunoRoutes.js
+++ routes/alunoRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Aluno = require('../models/aluno');
 const Matricula = require('../models/matricula');
+const database = require('../db');
 
 router.use(express.json());
 
@@ -99,6 +100,27 @@ router.delete("/:id", (req, resp) => {
                 });
             }
         })
+
+    } catch (e) {
+        console.log(e.toString());
+        resp.status(500).json({
+            statusCode: 500,
+            data: null,
+        });
+    }
+});
+
+router.get("/aluno_matricula/:id_curso", (req, resp) => {
+    const id = req.params.id_curso;
+    try {
+        database.query("SELECT alunos.id, alunos.nome FROM alunos where alunos.id NOT IN (SELECT matriculas.id_aluno FROM matriculas where matriculas.id_curso = " + id +")").
+            then((alunos) => {
+                resp.status(200).json({
+                    statusCode: 200,
+                    data: alunos[0],
+                });
+            });
+
 
     } catch (e) {
         console.log(e.toString());
